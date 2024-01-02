d470ee8fb7815350a126e8742eaa85642e0b4fe4
Lucas Jose de Freitas <99023129+LucasFre26@users.noreply.github.com>
2024-01-02 09:31:55 -0300

    Update variaveis.c

--- src/variaveis.c
+++ src/variaveis.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include <stdio.h> // Biblioteca padrão de entrada e saída
 
 // O compilador (GCC) faz a leitura do codigo fonte, verifica as bibliotecas importadas e gera um executavel para o SO que está executando
 
+// Uma função que a partir do main que se executa o programa em C é executado
 int main(void) {
 
   // Declaração e inicialização de variavel
@@ -16,3 +17,25 @@ int main(void) {
   
   return 0;
 }
+
+// Em um projeto so se tem um main #include <stdio.h> // Biblioteca padrão de entrada e saída
+
+// O compilador (GCC) faz a leitura do codigo fonte, verifica as bibliotecas importadas e gera um executavel para o SO que está executando
+
+// Uma função que a partir do main que se executa o programa em C é executado
+int main(void) {
+
+  // Declaração e inicialização de variavel
+  int idade = 0;
+
+  // Função para imprimir algo na tela
+  printf("Qual a sua idade: "); 
+
+  scanf("%d", &idade); // Receber dados
+
+  printf("\nA sua idade é %d", idade); // Saida de dados
+  
+  return 0;
+}
+
+// Em um projeto so se tem um main 
