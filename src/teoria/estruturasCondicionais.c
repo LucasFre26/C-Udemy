ebc22daaa6ed94883f060ba77f67ed6da7445747
Lucas Jose de Freitas <99023129+LucasFre26@users.noreply.github.com>
2024-01-02 10:28:08 -0300

    Update estruturasCondicionais.c

--- src/teoria/estruturasCondicionais.c
+++ src/teoria/estruturasCondicionais.c
@@ -1,14 +1,21 @@
+/* Estruturas condicionais if, else, else if
+                           se, entao, entao se  */
+
 #include <stdio.h>
 
 int main(void) {
 
+  // Declaracao de variaveis
   int idade = 0;
 
+  // Entrada
   printf("Digite a sua idade: ");
   scanf("%d", &idade);
 
+  // Saida
   printf("Sua idade e %d, então voce e", idade);
 
+  // Processamento e saida
   if(idade < 18){
     printf(" menor de idade.\n");
   } else if(idade > 18 && idade < 60){
