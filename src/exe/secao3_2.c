b64897f2676845eb208e26fc35178c70dc80552e
Lucas Jose de Freitas <99023129+LucasFre26@users.noreply.github.com>
2024-01-02 10:01:41 -0300

    Create secao3_2.c

---
+++ src/exe/secao3_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int num1 = 0, num2 = 0, num3 = 0;
+
+  printf("Digite o seu primeiro numero: ");
+  scanf("%d", &num1);
+
+  printf("Digite o seu segundo numero: ");
+  scanf("%d", &num2);
+
+  printf("Digite o seu terceiro numero: ");
+  scanf("%d", &num3);
+
+  printf("\n%d + %d + %d = %d", num1, num2, num3, num1 + num2 + num3);
+  
+  return 0;
+}
